1db649c3f52039c9916201e5909a95f915848aed
shirou.furui.j6a <shirou.furui.j6a@densotechno.com>
2025-03-10 14:35:54 +0900

    SoC要望対応
    MET
    ・MCU⇒SOCのLcomIF「SYS_DMEVRNGE」(EV航続可能距離機能有無）を有固定に変更
    BEV3CDCMET-168
    BEV3CDCMET-176

--- src/PE1VM1/App/MET/XSPI_MET/config/xspi_met_ch0.c
+++ src/PE1VM1/App/MET/XSPI_MET/config/xspi_met_ch0.c
@@ -590,6 +590,7 @@ static inline void    vd_s_XSpiCfgTxVariation(     U4 * u4_ap_pdu_tx) {
     u4_ap_pdu_tx[12] |= (U4)TRUE << 2;                                 /*  SYS_SW_OBBMODE           */ /* BEV SV1 provisionally */
     u4_ap_pdu_tx[12] |= (U4)TRUE << 3;                                 /*  SYS_ERM_ADU              */ /* BEV SV1 provisionally */
     u4_ap_pdu_tx[12] |= (U4)TRUE << 4;                                 /*  SYS_SW_OBBSAV            */ /* BEV SV1 provisionally */
+    u4_ap_pdu_tx[12] |= (U4)TRUE << 5;                                 /*  SYS_DMEVRNGE             */ /* BEV SV1 provisionally */
     u4_ap_pdu_tx[12] |= (U4)TRUE << 6;                                 /*  SYS_DMTOEC               */ /* BEV SV1 provisionally */
 
     u1_t_var_ecojdg_type = u1_g_VardefEcoJudge();
