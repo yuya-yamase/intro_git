cc0da9a821efaf132576ab3be11b3e2a2f334ea9
dt_kanji.ito.j2m <kanji.ito.j2m@densotechno.com>
2025-04-02 09:58:11 +0900

    CAN受信STUB無効化処理
    MET
    不要STUBヘッダの本体とインクルード部分を削除
    BEV3CDCMET-191

--- src/PE1VM1/App/MET/IpcApplication/Vom/Tripcom/sensor/config/tripsnsr_cfg.c
+++ src/PE1VM1/App/MET/IpcApplication/Vom/Tripcom/sensor/config/tripsnsr_cfg.c
@@ -20,7 +20,6 @@
 #include "oxcan.h"
 #if 0   /* BEV BSW provisionally */
 #else
-#include "Com_Cfg_STUB.h"
 #include "oxcan_channel_STUB.h"
 #endif
 #include "gpt_drv_frt.h"
--- src/PE1VM1/App/MET/Platform/ComplexDriver/Gateway/scc/gateway_cxpi_cfg_private.h
+++ src/PE1VM1/App/MET/Platform/ComplexDriver/Gateway/scc/gateway_cxpi_cfg_private.h
@@ -23,7 +23,6 @@
 #include "oxcan.h"
 #if 0   /* BEV BSW provisionally */
 #else
-#include "Com_Cfg_STUB.h"
 #include "oxcan_channel_STUB.h"
 #endif
 #include "veh_opemd.h"
