76b2535f3524dc570b30b643aedb81692159accb
dt_takaaki.otsuka.j5t <takaaki.otsuka.j5t@densotechno.com>
2025-09-12 14:05:59 +0900

    ソフト識別子、INTEG_VERSION更新
    Integ
    ソフト識別子、INTEG_VERSION更新
    BEVCDCFD-1814

--- src/PE3VM3/App/Product/config_1MOT/product_cfg.c
+++ src/PE3VM3/App/Product/config_1MOT/product_cfg.c
@@ -40,7 +40,7 @@
 #pragma ghs section rodata  = ".app_product_const"
 const U1    u1_gp_PRDCT_REL_VN[PRDCT_REL_VN_NBYTE] = {
     (U1)0x07U,
-    (U1)0x36U
+    (U1)0x3DU
 };
 #pragma ghs section rodata  = default
 
