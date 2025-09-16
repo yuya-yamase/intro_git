1258e9ef6a02f27e1d803b56a59c713be4ad7996
奥村泰地 <taichi.okumura.j7s@jp.denso.com>
2025-09-16 18:10:29 +0900

    ローカルPCからデバッグPCへ移動

--- src/PE3VM3/App/Central/esw/EthSwt/Config/EthSwt_Data.c
+++ src/PE3VM3/App/Central/esw/EthSwt/Config/EthSwt_Data.c
@@ -20,48 +20,61 @@
 #define D_ETHSWT_SWIC_ANDROID_PORT_ID           (7U)
 
 /* -------------------------------------------------------------------------- */
-static const uint8 S_ETHSWT_DATA_IDX_TABLE[D_ETHSWT_DATA_USE_PORT_NUM] =
-{
-    D_ETHSWT_SWIC_ADC_PORT_ID
-,   D_ETHSWT_SWIC_DCM_PORT_ID
-,   D_ETHSWT_SWIC_SAIL_PORT_ID
-,   D_ETHSWT_SWIC_TOOL_PORT_ID
-,   D_ETHSWT_SWIC_LINUX_PORT_ID
-,   D_ETHSWT_SWIC_ANDROID_PORT_ID
-};
+// static const uint8 S_ETHSWT_DATA_IDX_TABLE[D_ETHSWT_DATA_USE_PORT_NUM] =
+// {
+//     D_ETHSWT_SWIC_ADC_PORT_ID
+// ,   D_ETHSWT_SWIC_DCM_PORT_ID
+// ,   D_ETHSWT_SWIC_SAIL_PORT_ID
+// ,   D_ETHSWT_SWIC_TOOL_PORT_ID
+// ,   D_ETHSWT_SWIC_LINUX_PORT_ID
+// ,   D_ETHSWT_SWIC_ANDROID_PORT_ID
+// };
 /* -------------------------------------------------------------------------- */
-static S_ETHSWT_DATA_CHIPCOM G_ETHSWT_DATA;
-/* -------------------------------------------------------------------------- */
-static void ethswt_data_get_linkStatus(void);
+// static S_ETHSWT_DATA_CHIPCOM G_ETHSWT_DATA;
 
+static S_ETHSWT_DATA_LINK       G_ETHSWT_DATA_LINK;
+static S_ETHSWT_DATA_MIB        G_ETHSWT_DATA_MIB;
+static S_ETHSWT_DATA_SQI        G_ETHSWT_DATA_SQI;
+static S_ETHSWT_DATA_QCI        G_ETHSWT_DATA_QCI;
+/* -------------------------------------------------------------------------- */
+static void ethswt_data_set_link(void);
+static void ethswt_data_set_mib(void);
+static void ethswt_data_set_sqi(void);
+static void ethswt_data_set_qci(void);
+static void ethswt_data_set_state(void);
 /* -------------------------------------------------------------------------- */
 void EthSwt_Data_Init(void)
 {
-    LIB_memset((uint8*)&G_ETHSWT_DATA, 0, sizeof(G_ETHSWT_DATA));
+    // LIB_memset((uint8*)&G_ETHSWT_DATA, 0, sizeof(G_ETHSWT_DATA));
+
+    LIB_memset((uint8*)&G_ETHSWT_DATA_LINK, 0, sizeof(G_ETHSWT_DATA_LINK));
+    LIB_memset((uint8*)&G_ETHSWT_DATA_MIB, 0, sizeof(G_ETHSWT_DATA_MIB));
+    LIB_memset((uint8*)&G_ETHSWT_DATA_SQI, 0, sizeof(G_ETHSWT_DATA_SQI));
+    LIB_memset((uint8*)&G_ETHSWT_DATA_QCI, 0, sizeof(G_ETHSWT_DATA_QCI));
 
     return;
 }
 /* -------------------------------------------------------------------------- */
 void EthSwt_Data_LoProc(void)
 {
-    ethswt_data_get_linkStatus();
+    ethswt_data_set_linkStatus();
     
-    (void)ChipCom_SetPeriodicTxData(CHIPCOM_PERIODICID_ETHERSWT_MCUINFO, sizeof(G_ETHSWT_DATA), (uint8*)&G_ETHSWT_DATA);
+    // (void)ChipCom_SetPeriodicTxData(CHIPCOM_PERIODICID_ETHERSWT_MCUINFO, sizeof(G_ETHSWT_DATA), (uint8*)&G_ETHSWT_DATA);
 
     return;
 }
 /* -------------------------------------------------------------------------- */
-static void ethswt_data_get_linkStatus(void)
+static void ethswt_data_set_linkStatus(void)
 {
     uint8                   idx;
     EthTrcv_LinkStateType   status;
     Std_ReturnType          getResult;
 
-    for(idx = 0; idx < D_ETHSWT_DATA_USE_PORT_NUM; idx++) {
-        getResult = EthSwt_SWIC_GetLinkState(D_ETHSWT_SWIC_ID, S_ETHSWT_DATA_IDX_TABLE[idx], &status);
-        G_ETHSWT_DATA.portData[idx].linkStatus = status;
-        G_ETHSWT_DATA.portData[idx].linkGetResult = getResult;
-    }
+    // for(idx = 0; idx < D_ETHSWT_DATA_USE_PORT_NUM; idx++) {
+    //     getResult = EthSwt_SWIC_GetLinkState(D_ETHSWT_SWIC_ID, S_ETHSWT_DATA_IDX_TABLE[idx], &status);
+    //     G_ETHSWT_DATA.portData[idx].linkStatus = status;
+    //     G_ETHSWT_DATA.portData[idx].linkGetResult = getResult;
+    // }
 
     return;
 }
--- src/PE3VM3/App/Central/esw/EthSwt/Config/EthSwt_Data.h
+++ src/PE3VM3/App/Central/esw/EthSwt/Config/EthSwt_Data.h
@@ -8,27 +8,78 @@
 /* -------------------------------------------------------------------------- */
 #include <EthSwt_SWIC_Cfg.h>
 /* -------------------------------------------------------------------------- */
-#define D_ETHSWT_DATA_USE_PORT_NUM          (6U)
+#define D_ETHSWT_DATA_LINK_ADC_ID           (0U)
+#define D_ETHSWT_DATA_LINK_DCM_ID           (1U)
+#define D_ETHSWT_DATA_LINK_SAIL_ID          (2U)
+#define D_ETHSWT_DATA_LINK_TOOL_ID          (3U)
+#define D_ETHSWT_DATA_LINK_LINUX_ID         (4U)
+#define D_ETHSWT_DATA_LINK_ANDROID_ID       (5U)
+#define D_ETHSWT_DATA_LINK_NUM              (6U)
+
+#define D_ETHSWT_DATA_MIB_ADC_ID            (0U)
+#define D_ETHSWT_DATA_MIB_DCM_ID            (1U)
+#define D_ETHSWT_DATA_MIB_SAIL_ID           (2U)
+#define D_ETHSWT_DATA_MIB_TOOL_ID           (3U)
+#define D_ETHSWt_DATA_MIB_LINUX_ID          (4U)
+#define D_ETHSWT_DATA_MIB_ANDROID_ID        (5U)
+#define D_ETHSWT_DATA_MIB_NUM               (6U)
+
+#define D_ETHSWT_DATA_SQI_ADC_ID            (0U)
+#define D_ETHSWT_DATA_SQI_DCM_ID            (1U)
+#define D_ETHSWT_DATA_SQI_NUM               (2U)
+
+#define D_ETHSWT_DATA_QCI_DCM_TCP_ID        (0U)
+#define D_ETHSWT_DATA_QCI_DCM_UDP_ID        (1U)
+#define D_ETHSWT_DATA_QCI_DCM_ICMP_ID       (2U)
+#define D_ETHSWT_DATA_QCI_DLC_TCP_ID        (3U)
+#define D_ETHSWT_DATA_QCI_DLC_UDP_ID        (4U)
+#define D_ETHSWT_DATA_QCI_NUM               (5U)
 /* -------------------------------------------------------------------------- */
 #pragma pack(1)
+/* -------------------------------------------------------------------------- */
 typedef struct {
-    Eth_ModeType            portMode;                               /* 4byte */
-    uint8                   indMode;                                /* 1byte */
-    EthTrcv_LinkStateType   linkStatus;                             /* 1byte */
-    Std_ReturnType          linkGetResult;                          /* 1byte */
-    uint8                   frameDiscardFactor[5];                  /* 5byte */
-    uint32                  ingressDiscardNum;                      /* 4byte */
-    uint32                  egressDiscardNum;                       /* 4byte */
-    uint8                   tcamInfo[4];                            /* 4byte */
-    uint8                   flowMeterInfo[4];                       /* 4byte */
-    uint8                   sqiValue;                               /* 1byte */
-    uint8                   sqiGetResult;                           /* 1byte */
-} S_ETHSWT_DATA_FOR_PORT;                                           /* Total: 30byte */
+    Std_ReturnType                  linkGetResult;                          /* 1byte */
+    EthTrcv_LinkStateType           linkStatus;                             /* 1byte */
+} S_ETHSWT_DATA_LINK_PER_PORT;                                              /* Total: 2byte */
 
 typedef struct {
-    EthSwt_StateType        ethswtStatus;                           /* 1byte */
-    S_ETHSWT_DATA_FOR_PORT  portData[D_ETHSWT_DATA_USE_PORT_NUM];   /* 180byte */ /* idx = 0:A-DC, 1:DCM, 2:SAIL, 3:Tool, 4:Linux, 5:Android */
-} S_ETHSWT_DATA_CHIPCOM;                                            /* Total: 181byte */
+    S_ETHSWT_DATA_LINK_PER_PORT     link[D_ETHSWT_DATA_LINK_NUM];           /* 12byte */
+    uint32                          id;                                     /* 4byte */
+} S_ETHSWT_DATA_LINK;                                                       /* Total: 16byte */
+/* -------------------------------------------------------------------------- */
+typedef struct {
+    uint32                          InFCSErr;                               /* 4byte */
+    uint32                          InDiscards;                             /* 4byte */
+    uint32                          InFiltered;                             /* 4byte */
+    uint32                          InOversize;                             /* 4byte */
+    uint32                          InUndersize;                            /* 4byte */
+    uint32                          Tcam0;                                  /* 4byte */
+    uint32                          Tcam1;                                  /* 4byte */
+    uint32                          Tcam2;                                  /* 4byte */
+    uint32                          Tcam3;                                  /* 4byte */
+    uint32                          reserved;                               /* 4byte */
+} S_ETHSWT_DATA_MIB_PER_PORT;                                               /* Total: 20byte */
+
+typedef struct {
+    S_ETHSWT_DATA_MIB_PER_PORT      mib[D_ETHSWT_DATA_MIB_NUM];             /* 240byte */
+    uint32                          id;                                     /* 4byte */
+} S_ETHSWT_DATA_MIB;                                                        /* Total: 244byte */
+/* -------------------------------------------------------------------------- */
+typedef struct {
+    Std_ReturnType                  sqiGetResult;                           /* 1byte */
+    uint8                           sqiValue;                               /* 1byte */   
+} S_ETHSWT_DATA_SQI_PER_PORT;                                               /* Total: 2byte */
+
+typedef struct {
+    S_ETHSWT_DATA_SQI_PER_PORT      sqi[D_ETHSWT_DATA_SQI_NUM];             /* 4byte */
+    uint32                          id;                                     /* 4byte */
+} S_ETHSWT_DATA_SQI;                                                        /* Total: 8byte */
+/* -------------------------------------------------------------------------- */
+typedef struct {
+    uint32                          qciCount[D_ETHSWT_DATA_QCI_NUM];        /* 20byte */
+    uint32                          id;                                     /* 4byte */
+} S_ETHSWT_DATA_QCI;                                                        /* Total: 24byte */
+/* -------------------------------------------------------------------------- */
 #pragma pack()
 /* -------------------------------------------------------------------------- */
 void EthSwt_Data_Init(void);
