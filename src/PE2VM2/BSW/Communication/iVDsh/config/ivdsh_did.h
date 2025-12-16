7b06ccd6d74a45a04e065ed4231dabe40ca6b03f
dt_kanji.ito.j2m <kanji.ito.j2m@densotechno.com>
2025-12-16 16:58:51 +0900

    リプロ対応
    MET,IVI
    develop用にiVDshを正式対応
    BEV3CDCMET-2508

--- src/PE2VM2/BSW/Communication/iVDsh/config/ivdsh_did.h
+++ src/PE2VM2/BSW/Communication/iVDsh/config/ivdsh_did.h
@@ -24,7 +24,7 @@
 /*-----------------------------------------------------------------------------------------------------------------------------------*/
 /*  Literal Definitions                                                                                                              */
 /*-----------------------------------------------------------------------------------------------------------------------------------*/
-#define IVDSH_NUM_DID_WRI                        (21U)
+#define IVDSH_NUM_DID_WRI                        (23U)
 
 /*-------------------------------------------------------------------------------*/
 /* CPREQ_XXX = "https://wiki.geniie.net/x/Wcq5pQ" numbering list (sample config) */
@@ -50,34 +50,37 @@
 #define IVDSH_DID_WRI_VM2TO1_MET1S43             (18U)
 #define IVDSH_DID_WRI_VM2TO1_MET1S62             (19U)
 #define IVDSH_DID_WRI_VM2TO1_MET1S70             (20U)
+#define IVDSH_DID_WRI_FWUPXREQ_H                 (21U)
+#define IVDSH_DID_WRI_FWUPXREQ_D                 (22U)
 
 /*-----------------------------------------------------------------------------------------------------------------------------------*/
-#define IVDSH_NUM_DID_REA                        (21U)
+#define IVDSH_NUM_DID_REA                        (22U)
 
 /* Read from VM#0 */
-#define IVDSH_DID_REA_CANBUS_STS_2M1             (21U)
-#define IVDSH_DID_REA_CANBUS_STS_5M              (22U)
-#define IVDSH_DID_REA_CANBUS_STS_2M2             (23U)
-#define IVDSH_DID_REA_CANBUS_STS_LCAN            (24U)
+#define IVDSH_DID_REA_CANBUS_STS_2M1             (23U)
+#define IVDSH_DID_REA_CANBUS_STS_5M              (24U)
+#define IVDSH_DID_REA_CANBUS_STS_2M2             (25U)
+#define IVDSH_DID_REA_CANBUS_STS_LCAN            (26U)
 /* Read from VM#1 */
-#define IVDSH_DID_REA_VM1TO2_TIMOFST             (25U)
-#define IVDSH_DID_REA_VM1TO2_TIMFMT              (26U)
-#define IVDSH_DID_REA_VM1TO2_CAL                 (27U)
-#define IVDSH_DID_REA_VM1TO2_CALDEF              (28U)
-#define IVDSH_DID_REA_VM1TO2_CALMIN              (29U)
-#define IVDSH_DID_REA_VM1TO2_CAM_DIAP            (30U)
-#define IVDSH_DID_REA_VM1TO2_FLYNOP              (31U)
-#define IVDSH_DID_REA_VM1TO23_FSLP               (32U)
-#define IVDSH_DID_REA_VM1TO2_WHLINI_RES          (33U)
-#define IVDSH_DID_REA_VM1TO2_MET1D51             (34U)
+#define IVDSH_DID_REA_VM1TO2_TIMOFST             (27U)
+#define IVDSH_DID_REA_VM1TO2_TIMFMT              (28U)
+#define IVDSH_DID_REA_VM1TO2_CAL                 (29U)
+#define IVDSH_DID_REA_VM1TO2_CALDEF              (30U)
+#define IVDSH_DID_REA_VM1TO2_CALMIN              (31U)
+#define IVDSH_DID_REA_VM1TO2_CAM_DIAP            (32U)
+#define IVDSH_DID_REA_VM1TO2_FLYNOP              (33U)
+#define IVDSH_DID_REA_VM1TO23_FSLP               (34U)
+#define IVDSH_DID_REA_VM1TO2_WHLINI_RES          (35U)
+#define IVDSH_DID_REA_VM1TO2_MET1D51             (36U)
+#define IVDSH_DID_REA_FWUPXRES                   (37U)
 /* Read from VM#3 */
-#define IVDSH_DID_REA_VM3TO2_SIPERR_INF          (35U)
-#define IVDSH_DID_REA_VM3TO2_OTA_OFFSTS          (36U)
-#define IVDSH_DID_REA_VM3TO2_BOOTLOG_INF         (37U)
-#define IVDSH_DID_REA_VM3TO12_RESTART            (38U)
-#define IVDSH_DID_REA_VM3TO2_BOOT_CNT            (39U)
-#define IVDSH_DID_REA_VM3TO2_BOOT_TIME           (40U)
-#define IVDSH_DID_REA_GPS_STS                    (41U)
+#define IVDSH_DID_REA_VM3TO2_SIPERR_INF          (38U)
+#define IVDSH_DID_REA_VM3TO2_OTA_OFFSTS          (39U)
+#define IVDSH_DID_REA_VM3TO2_BOOTLOG_INF         (40U)
+#define IVDSH_DID_REA_VM3TO12_RESTART            (41U)
+#define IVDSH_DID_REA_VM3TO2_BOOT_CNT            (42U)
+#define IVDSH_DID_REA_VM3TO2_BOOT_TIME           (43U)
+#define IVDSH_DID_REA_GPS_STS                    (44U)
 
 /*-----------------------------------------------------------------------------------------------------------------------------------*/
 #define IVDSH_NUM_DID                            (IVDSH_NUM_DID_WRI + IVDSH_NUM_DID_REA)
