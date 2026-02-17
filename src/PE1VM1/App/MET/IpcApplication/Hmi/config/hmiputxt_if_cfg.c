8a16414d6bdc6e8a1f9f628312d3fd6b6439045e
ohashi_ryo <ryou.oohashi.j8x@densotechno.com>
2026-02-17 14:31:19 +0900

    FF2 B_SECBDC対応/メッセージ認証用MACキー未更新警告の追加
    (特異点)
    入力信号は+B系かつ
    途絶時制御がIG-ON:前回値保持、IG-OFF:非表示
    
    Alert
    B_SECBDCのMID表示要求の判定処理を追加
    IG-ON又はIG-OFF25(m)未満と+B電源としてのフレーム受信状態と信号値でu4_src_chk処理を行い、真理値表に合わせて要求有無を判定する処理の追加
    +B電源のフレーム受信状態については補正関数処理の追加
    Hmi
    B_SECBDCのMID表示要求の判定処理を追加
    BEV3CDCMET-3797

--- src/PE1VM1/App/MET/IpcApplication/Hmi/config/hmiputxt_if_cfg.c
+++ src/PE1VM1/App/MET/IpcApplication/Hmi/config/hmiputxt_if_cfg.c
@@ -229,6 +229,7 @@ const ST_ALERT_REQBIT   st_gp_HMIPUTXTREQBIT_SLOTC[HMIPUTXTCFG_IF_SLOTC_NUM] = {
 
 const ST_ALERT_REQBIT   st_gp_HMIPUTXTREQBIT_SLOTD[HMIPUTXTCFG_IF_SLOTD_NUM] = {
     {    (U2)ALERT_CH_M_SECMSG,             (U1)ALERT_REQ_M_SECMSG_UNREGISTERED,    (U1)0U,     (U4)0x01000000U    },
+    {    (U2)ALERT_CH_B_SECBDC,             (U1)ALERT_REQ_B_SECBDC_UNREGISTERED,    (U1)0U,     (U4)0x01000000U    },
     {    (U2)ALERT_CH_O_CHAMAL,             (U1)ALERT_REQ_O_CHAMAL_MALFUNC,         (U1)2U,     (U4)0x00400000U    },
     {    (U2)ALERT_CH_O_CHAMAL,             (U1)ALERT_REQ_O_CHAMAL_MALFUNC_BZ,      (U1)2U,     (U4)0x00400000U    },
     {    (U2)ALERT_CH_D_SBW_PD_RIS,         (U1)ALERT_REQ_D_SBW_PD_RIS_MSG_12,      (U1)4U,     (U4)0x00000004U    },
@@ -343,6 +344,9 @@ U2 u2_g_HmiputxtSizeReqbit(void){
 /*  BEV-13   02/10/2026  KO       Change for BEV FF2.(MET-M_SECMSG-CSTD-0-01-A-C0)                                                   */
 /*  BEV-14   01/15/2026  HT       Change for Full_function2 (MET-M_REMWAR-CSTD-2-04-A-C0)                                            */
 /*                                Removed "ALERT_REQ" in order to transfer signal transmission control from the MCU to the SoC       */
+/*  BEV-15   02/06/2026  HY       Change config for BEV Full_Function_2.                                                             */
+/*                                MET-B_SECBDC-CSTD-0-00-A-C0                                                                        */
+/*                                Changed due to specification change from feature to legacy.                                        */
 /*                                                                                                                                   */
 /*  * TA   = Teruyuki Anjima, Denso                                                                                                  */
 /*  * TH   = Takahiro Hirano, Denso Techno                                                                                           */
@@ -358,5 +362,6 @@ U2 u2_g_HmiputxtSizeReqbit(void){
 /*  * KH   = Kiko Huerte,     DTPH                                                                                                   */
 /*  * SN   = Shizuka Nakajima, KSE                                                                                                   */
 /*  * HT   = Hibiki Tanii, KSE                                                                                                       */
+/*  * YH   = Yuki Hatakeyama, KSE                                                                                                    */
 /*                                                                                                                                   */
 /*===================================================================================================================================*/
